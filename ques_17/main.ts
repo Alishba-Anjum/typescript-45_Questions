efc7d0bd6eaf7732d0a0f2b79753eab26bfa24bd
Alishba-Anjum <alishbaahmedkhan11@gmail.com>
2024-05-31 01:15:06 +0500

    ques_17 added

---
+++ ques_17/main.ts
@@ -0,0 +1,4 @@
+let places: string[] = ['Maldives', 'Skardu', 'Japan', 'Switzerland', 'Singapore'];
+console.log('orignal order:', places);
+console.log('Alphabetical order:', [...places].sort());
+console.log('reversed Alphabetical order:',[...places].sort().reverse());
