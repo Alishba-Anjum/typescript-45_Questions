fc91b9f9f77218aabd6829666c30f86c38698fbe
Alishba-Anjum <alishbaahmedkhan11@gmail.com>
2024-06-01 22:30:50 +0500

    ques_18 added

---
+++ ques_18/main.js
@@ -0,0 +1,2 @@
+var guest = ['Muniba', 'Riya', 'Hoor', 'Rafiya'];
+console.log("i'm inviting ".concat(guest.length, " people to dinner"));
---
+++ ques_18/main.ts
@@ -0,0 +1,3 @@
+let guest: string[] = ['Muniba', 'Riya', 'Hoor', 'Rafiya'];
+
+console.log(`i'm inviting ${guest.length} people to dinner`);
