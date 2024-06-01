d20dc3ad9fb4f9a742d6ac7b646d0657b40a2cc4
Alishba-Anjum <alishbaahmedkhan11@gmail.com>
2024-06-02 02:26:27 +0500

    ques_21 added

---
+++ ques_21/main.js
@@ -0,0 +1,4 @@
+var Actors = ['Shahid kapoor', 'Feroz khan', 'Wahaj ali', 'Salman khan'];
+console.log(Actors[4]);
+Actors[4] = "Ranbir kapoor";
+console.log(Actors);
---
+++ ques_21/main.ts
@@ -0,0 +1,5 @@
+let Actors: string[] = ['Shahid kapoor', 'Feroz khan', 'Wahaj ali', 'Salman khan'];
+console.log(Actors[4]);
+Actors[4] = "Ranbir kapoor";
+console.log(Actors);
+
