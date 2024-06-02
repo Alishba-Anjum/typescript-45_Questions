e80ab3c3bc995175a564c1fe96bc0f8a3a091eda
Alishba-Anjum <alishbaahmedkhan11@gmail.com>
2024-06-02 23:00:47 +0500

    commit

---
+++ ques_23/main.js
@@ -0,0 +1,15 @@
+var Name = 'Rohaan';
+console.log(Name == "Rohaan"); //true
+console.log(Name == "Alishba"); //false
+var favDish = "Biryani";
+console.log(favDish == "Biryani"); //true
+console.log(favDish == "pasta"); //false
+var number = 5;
+console.log(number == 5); //true
+console.log(number == 8); //false
+var favMovie = "Brahmastra";
+console.log(favMovie == "Brahmastra"); //true
+console.log(favMovie == 'PK'); //false
+var game = "Candy crush";
+console.log(game == "Candy crush"); //true
+console.log(game == "pubg"); //true
---
+++ ques_23/main.ts
@@ -0,0 +1,29 @@
+let Name:string = 'Rohaan';
+
+console.log(Name == "Rohaan");//true
+console.log(Name == "Alishba");//false
+
+let favDish:string = "Biryani";
+
+console.log(favDish == "Biryani");//true
+console.log(favDish =="pasta");//false
+
+let number: number = 5
+
+console.log(number == 5);//true
+console.log(number == 8);//false
+
+let  favMovie: string = "Brahmastra";
+
+console.log(favMovie == "Brahmastra");//true
+console.log(favMovie == 'PK');//false
+
+let game: string = "Candy crush";
+
+console.log(game == "Candy crush");//true
+console.log(game == "pubg");//true
+
+
+
+
+
