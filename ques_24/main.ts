69df314ce5134522b1c2bf21eeb4b40bd0585b06
Alishba-Anjum <alishbaahmedkhan11@gmail.com>
2024-06-04 04:11:02 +0500

    ques_24 added

---
+++ ques_24/main.ts
@@ -0,0 +1,28 @@
+// Equality  with strings
+let name1: string = "alishba";
+console.log("testing equality with strings:");
+console.log("alishba" == "alishba"); //true
+console.log("Alishba" == "alishba");//false
+
+// Using the lowerCase function
+console.log("Testing with lower Case:");
+console.log("Alishba".toLowerCase() == "alishba");//true
+
+//Numerical test
+console.log("Numerical test:");
+console.log(9 > 8);//true
+console.log(16< 10);//false
+
+//Test using && and || operators
+console.log("tests with && and ||:");
+console.log((10 > 5) && (2 < 1));//false
+console.log((12 < 11) || (15 > 10));//true
+
+//Test whether an item is in array
+let fruits = ["apple", "banana", "orange"];
+console.log("is 'apple' in fruits? ");
+console.log(fruits.includes("apple"));//true
+
+//test whether an item is not ina array
+console.log("is 'mango' not in fruits?");
+console.log(!fruits.includes("mango"));//true
