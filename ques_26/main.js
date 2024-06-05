684388544a3ed865f31384dbb9e5bcb280954e18
Alishba-Anjum <alishbaahmedkhan11@gmail.com>
2024-06-06 02:17:48 +0500

    ques_26 added

---
+++ ques_26/main.js
@@ -0,0 +1,17 @@
+//if _ else conditionals
+var aleinColor = "pink";
+if (aleinColor == "pink") {
+    console.log("you just earned 10 points!");
+}
+else {
+    console.log("you lost!");
+}
+;
+var alein_color = "green";
+if (alein_color == "red") {
+    console.log("you just earned 100 points!");
+}
+else {
+    console.log("you lost!");
+}
+;
---
+++ ques_26/main.ts
@@ -0,0 +1,16 @@
+//if _ else conditionals
+let aleinColor:string  = "pink";
+
+if (aleinColor == "pink"){
+    console.log("you just earned 10 points!");
+}else{
+    console.log("you lost!")
+};
+
+let alein_color:string  = "green";
+
+if (alein_color == "red"){
+    console.log("you just earned 100 points!");
+}else{
+    console.log("you lost!")
+};
