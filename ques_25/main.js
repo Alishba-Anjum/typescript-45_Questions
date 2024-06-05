2290f8a972a2c362a0b7f5cb02bb33efa9faa818
Alishba-Anjum <alishbaahmedkhan11@gmail.com>
2024-06-06 02:16:20 +0500

    ques_25 added

---
+++ ques_25/main.js
@@ -0,0 +1,4 @@
+var aleinColor = "Black";
+if (aleinColor == "Black") {
+    console.log("you just earned 5 poitns!");
+}
---
+++ ques_25/main.ts
@@ -0,0 +1,4 @@
+let  aleinColor: string = "Black";
+if (aleinColor == "Black") {
+    console.log("you just earned 5 poitns!");
+}
