60a3005501769f710d829355b792a94093e689b8
Alishba-Anjum <alishbaahmedkhan11@gmail.com>
2024-06-06 02:31:44 +0500

    ques_28 added

---
+++ ques_28/main.js
@@ -0,0 +1,17 @@
+// nestes if _else
+var age = 28;
+if (age < 2) {
+    console.log("The person is a baby.");
+}
+else if (age < 10) {
+    console.log("The persons is a kid.");
+}
+else if (age < 19) {
+    console.log("The person ia a teenager.");
+}
+else if (age < 40) {
+    console.log("The person is a adult.");
+}
+else {
+    console.log("The person i an elder");
+}
