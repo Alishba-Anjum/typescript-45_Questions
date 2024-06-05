edca443696bf19d7592cf2934bd9c833ece7be77
Alishba-Anjum <alishbaahmedkhan11@gmail.com>
2024-06-06 02:29:17 +0500

    ques_27 added

---
+++ ques_27/main.ts
@@ -0,0 +1,23 @@
+let alein_color = 'green';
+
+if(alein_color == "green"){
+    console.log('you earned 5 points');
+}else{
+    console.log("you lost");
+
+}
+alein_color = "yellow";
+
+if(alein_color == "yellow"){
+    console.log("you earned 10 points");
+}else{
+    console.log("you lost"); 
+}
+
+alein_color = "red";
+if(alein_color == "red"){
+    console.log('you earned 15 points');   
+}else{
+    console.log('you lost');
+    
+}
